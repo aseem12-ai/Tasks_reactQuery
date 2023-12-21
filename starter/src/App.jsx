29df5fb751d29bb30a7d9daa31778162b24d9254
Mohammad Aseem <aseemmohammad73@gmail.com>
2023-12-21 10:27:51 +0530

    s

---
+++ starter/src/App.jsx
@@ -0,0 +1,22 @@
+import { ToastContainer } from 'react-toastify';
+
+import Form from './Form';
+import Items from './Items';
+
+// const defaultItems = [
+//   { id: nanoid(), title: 'walk the dog', isDone: false },
+//   { id: nanoid(), title: 'wash dishes', isDone: false },
+//   { id: nanoid(), title: 'drink coffee', isDone: true },
+//   { id: nanoid(), title: 'take a nap', isDone: false },
+// ];
+const App = () => {
+  
+  return (
+    <section className='section-center'>
+      <ToastContainer position='top-center' />
+      <Form />
+      <Items  />
+    </section>
+  );
+};
+export default App;
